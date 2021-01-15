76c40430c470309ce8444d4f8cd7e843afbef4e5
Suwat Inkaew <swik999@gmail.com>
2021-01-15 15:22:59 +0700

    change

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
 
+long long int fibonacci(long long int);
+
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+
+    cout << fibonacci(50);
+
+    return 0;
+}
+
+long long int fibonacci(long long int x){
+	
+	if(x == 0){
+		return 0;
+	}else if(x == 1){
+		return 1;
+	}else{
+		return fibonacci(x-1) + fibonacci(x-2);
+	}
 }
